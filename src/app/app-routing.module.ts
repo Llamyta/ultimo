930a11bad3f1f56a5181476c9263e5666a711a9b
Berny Serrano <mbernardaserrano@gmail.com>
2019-12-09 04:33:24 -0400

    cambios diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { HomeComponent } from './components/home/home.component';
 import { RequisitosComponent } from './components/requisitos/requisitos.component';
 import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 import { PuntosdonacionComponent } from './components/puntosdonacion/puntosdonacion.component';
+import { SolicitardonadoresComponent } from './components/solicitardonadores/solicitardonadores.component';
+import { DonacionorganosComponent } from './components/donacionorganos/donacionorganos.component';
+
+
 
 
 
@@ -13,6 +17,8 @@ const routes: Routes = [
    { path: 'requisitos', component: RequisitosComponent},
    { path: 'registrarse', component: RegistrarseComponent},
    { path: 'pntdon', component: PuntosdonacionComponent},
+   { path: 'solicitadonadores', component: SolicitardonadoresComponent},
+   { path: 'organos', component: DonacionorganosComponent},
    { path: '**', pathMatch: 'full', component: HomeComponent},
 ];
 
