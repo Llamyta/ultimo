65f28ecbc4712f22833a5e709577d98ed14a987b
Luis Fernando Arevalo Antezana <clamth01@gmail.com>
2019-12-14 12:36:04 -0400

    Modificando Usuario

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -1,40 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { CitasHospitalService } from '../../services/citas-hospital.service';
-import { CitasService } from '../../services/citas.service';
-import { DataApiService } from '../../services/data-api.service';
-import { AuthService } from '../../services/auth.service';
-
+import { Component, OnInit } from "@angular/core";
+import { CitasHospitalService } from "../../services/citas-hospital.service";
+import { CitasService } from "../../services/citas.service";
+import { DataApiService } from "../../services/data-api.service";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
-  selector: 'app-usuario',
-  templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.css']
+  selector: "app-usuario",
+  templateUrl: "./usuario.component.html",
+  styleUrls: ["./usuario.component.css"]
 })
 export class UsuarioComponent implements OnInit {
-
   ciPersona: any;
-  citasHospitalesvar: any[]=[];  
-  constructor(public citasHospitales:CitasHospitalService, public citaspersonas:CitasService, public persona: DataApiService, private authService: AuthService) { }
+  citasHospitalesvar: any[] = [];
+
+  personaCompleta: any[]=[];
+  fotoPersona: any;
+  constructor(
+    public citasHospitales: CitasHospitalService,
+    public citaspersonas: CitasService,
+    public persona: DataApiService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.authService.isAuth().subscribe(user => {
-     this.persona.obtenerPersonaRegistro().subscribe(resp => {
-       resp.forEach(element => {                 
-         if (user.email == element.email) {                      
-           this.ciPersona = element.ci;                      
-           this.citasHospitales.obtenerCitasRegistro().subscribe(citasHosp =>{             
-             citasHosp.forEach(element2 => {
-               if (this.ciPersona == element2.id_usuario) {
-                 this.citasHospitalesvar.push(element2);
-                 console.log(this.citasHospitalesvar);
-                 
-               }
-             });
-           })
-         }  
-       });
-     })
-    })        
-  }
+      this.fotoPersona = user.photoURL;
+      console.log(this.fotoPersona);
 
+      // recupera hospitales
+      this.persona.obtenerPersonaRegistro().subscribe(resp => {
+        resp.forEach(element => {
+          if (user.email == element.email) {
+            this.personaCompleta.push(element);
+            this.ciPersona = element.ci;
+            this.citasHospitales.obtenerCitasRegistro().subscribe(citasHosp => {
+              citasHosp.forEach(element2 => {
+                if (this.ciPersona == element2.id_usuario) {
+                  this.citasHospitalesvar.push(element2);                  
+                }
+              });
+            });
+          }
+        });
+      });
+
+      //  recupera citas personales
+      this.persona.obtenerPersonaRegistro().subscribe(resp => {
+        resp.forEach(element => {
+          if (user.email == element.email) {
+            this.ciPersona = element.ci;
+            this.citaspersonas.obtenerCitasRegistro().subscribe(citasHosp => {
+              citasHosp.forEach(element2 => {
+                if (this.ciPersona == element2.id_usuario) {
+                  this.citasHospitalesvar.push(element2);
+                  console.log(this.citasHospitalesvar);
+                }
+              });
+            });
+          }
+        });
+      });
+    });
+  }
 }
