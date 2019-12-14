efde4b4c539fd927250be72b53a94e984e0aba29
Luis Fernando Arevalo Antezana <clamth01@gmail.com>
2019-12-14 12:04:16 -0400

    usuarios recupera cita y muestra

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CitasHospitalService } from '../../services/citas-hospital.service';
+import { CitasService } from '../../services/citas.service';
+import { DataApiService } from '../../services/data-api.service';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-usuario',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioComponent implements OnInit {
 
-  constructor() { }
+  ciPersona: any;
+  citasHospitalesvar: any[]=[];  
+  constructor(public citasHospitales:CitasHospitalService, public citaspersonas:CitasService, public persona: DataApiService, private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.isAuth().subscribe(user => {
+     this.persona.obtenerPersonaRegistro().subscribe(resp => {
+       resp.forEach(element => {                 
+         if (user.email == element.email) {                      
+           this.ciPersona = element.ci;                      
+           this.citasHospitales.obtenerCitasRegistro().subscribe(citasHosp =>{             
+             citasHosp.forEach(element2 => {
+               if (this.ciPersona == element2.id_usuario) {
+                 this.citasHospitalesvar.push(element2);
+                 console.log(this.citasHospitalesvar);
+                 
+               }
+             });
+           })
+         }  
+       });
+     })
+    })        
   }
 
 }
