b2bdbef63acb74a71d076da1f7c33eb6ae03b7a6
Luis Fernando Arevalo Antezana <clamth01@gmail.com>
2019-12-17 20:54:57 -0400

    comiit test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -54,6 +54,7 @@ import { AprobarcitasComponent } from './componets/aprobarcitas/aprobarcitas.com
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CommonModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule,
